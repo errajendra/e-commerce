5163ebfccddc890f44c64aef31fc2eafaffa51eb
Rajendra <rajinfotech.devoloper@gmail.com>
2024-10-19 19:39:06 +0530

    allow hosts

--- eCommerce/settings.py
+++ eCommerce/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-9=3w3#+(-3iz)+gkb*plzrhsuk#bw=matlpvvx3kyu#b938hp=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['muscleayurveda.com']
 
 
 # Application definition
